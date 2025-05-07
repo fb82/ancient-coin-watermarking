a254b7bcf26456f365ac01553e506ce0a3b8dd9b
WaRLoCK <warlock@honey-gold>
2025-05-07 18:08:21 +0200

    Update .gitignore

--- runme.py
+++ runme.py
@@ -1,11 +1,12 @@
 import os
 import sys
-from os.path import expanduser
 from PIL import Image
 import cv2
 import matplotlib.pyplot as plt
 import matplotlib.patches as pplt
 import numpy as np
+import wget
+import argparse
 
 from calculate_PSNR_SSIM import calculate_psnr as psnr
 from calculate_PSNR_SSIM import calculate_ssim as ssim
@@ -13,6 +14,7 @@ from calculate_PSNR_SSIM import calculate_ssim as ssim
 import utils_img
 import torch
 from torchvision.transforms import functional
+from torchvision.transforms import ToPILImage
 from augly.image import functional as aug_functional
 
 def list_images(path):
@@ -108,10 +110,16 @@ def get_coin_image(image, show=False, box_th=0.98):
 from trustmark import TrustMark
 
 class trustmark_watermarking:
-    def __init__(self, mode='P'):
+    def __init__(self, **args):
         # Available modes: Q=balance, P=high visual quality, C=compact decoder, B=base from paper
         
+        mode='P'
+        if 'mode' in args: mode = args['mode']
+        
         self.tm=TrustMark(verbose=False, model_type=mode, encoding_type=TrustMark.Encoding.BCH_5)
+
+        self.wm_l = 30        
+        if "wm_l" in args: self.wm_l = args["wm_l"]
 
         
     def name(self):
@@ -130,7 +138,7 @@ class trustmark_watermarking:
         has_alpha = cover.mode == 'RGBA'
         if (has_alpha): alpha = cover.split()[-1]
         
-        capacity = self.tm.schemaCapacity()
+        # capacity = self.tm.schemaCapacity()
         bitstring = orig_w
         encoded = self.tm.encode(rgb, bitstring, MODE='binary')
         
@@ -141,6 +149,9 @@ class trustmark_watermarking:
         encoded.save(outfile, exif=cover.info.get('exif'), icc_profile=cover.info.get('icc_profile'), dpi=cover.info.get('dpi'))
 
         tile = cv2.imread('tmp1.png', cv2.IMREAD_COLOR)
+        
+        os.remove('tmp0.png')          
+        os.remove('tmp1.png')            
         
         return tile
     
@@ -155,6 +166,8 @@ class trustmark_watermarking:
         
         if wm_present:
             wm_l = args['wm_l']
+            if wm_l is None: wm_l = self.wm_l
+            
             return [True if wm_secret[i] == '1' else False for i in range(wm_l)]            
         else:
             return None
@@ -166,7 +179,12 @@ class blind_watermarking:
     def name(self):
         return "blind watermarking"
     
-    
+    def __init__(self, **args):    
+        self.wm_l = 30
+        
+        if "wm_l" in args: self.wm_l = args["wm_l"]
+
+            
     def embed(self, tile, **args):
         cv2.imwrite('tmp0.png', tile)        
         wm = args['wm']
@@ -178,34 +196,178 @@ class blind_watermarking:
     
         tile = cv2.imread('tmp1.png', cv2.IMREAD_COLOR)
         
+        os.remove('tmp0.png')          
+        os.remove('tmp1.png')          
+        
         return tile
     
     
     def extract(self, tile, **args):
         cv2.imwrite('aux0.png', tile)        
-        wm_l = args['wm_l']
-      
+
+        wm_l = None
+        if "wm_l" in args: wm_l = args["wm_l"]
+        if wm_l is None: wm_l = self.wm_l
+        
         bwm1 = WaterMark(password_img=1, password_wm=1)
-        return bwm1.extract('aux0.png', wm_shape=wm_l, mode='bit')
+        wtm = bwm1.extract('aux0.png', wm_shape=wm_l, mode='bit')
+
+        os.remove('aux0.png')
+
+        return wtm
 
 
 conf_path = os.path.split(__file__)[0]
 sys.path.append(os.path.join(conf_path, '../ssl_watermarking'))
     
 import encode as sslw_encode
-import evaluate as sslw_evaluate
+import decode as sslw_decode
+import data_augmentation as sslw_data_augmentation
 import utils as sslw_utils
 import utils_img as sslw_utils_img
-
 
 class ssl_watermarking:
     def name(self):
         return "ssl watermarking"    
 
     def __init__(self, **args):
-        print("todo")
-    
+        # Set seeds for reproductibility
+        torch.manual_seed(0)
+        np.random.seed(0)
+        
+        os.makedirs("sslw/models", exist_ok=True)
+        os.makedirs("sslw/normlayers", exist_ok=True)
+        
+        to_check = [
+            [
+                "sslw/models/dino_r50_plus.pth",
+                "https://dl.fbaipublicfiles.com/ssl_watermarking/dino_r50_plus.pth",
+            ],
+            [            
+                "sslw/normlayers/out2048_yfcc_orig.pth",
+                "https://dl.fbaipublicfiles.com/ssl_watermarking/out2048_yfcc_orig.pth",
+            ],
+            [
+                "sslw/normlayers/out2048_yfcc_resized.pth",
+                "https://dl.fbaipublicfiles.com/ssl_watermarking/out2048_yfcc_resized.pth",
+            ],
+            [
+                "sslw/normlayers/out2048_coco_orig.pth",
+                "https://dl.fbaipublicfiles.com/ssl_watermarking/out2048_coco_orig.pth",
+            ],
+            [
+                "sslw/normlayers/out2048_coco_resized.pth",
+                "https://dl.fbaipublicfiles.com/ssl_watermarking/out2048_coco_resized.pth",
+            ],
+        ]
+        
+        for file, link in to_check:
+            if not os.path.isfile(file):
+                wget.download(link, out=file)
+            
+        self.model_path = "sslw/models/dino_r50_plus.pth"
+        self.model_name = 'resnet50'
+        self.normlayer_path = "sslw/normlayers/out2048_yfcc_orig.pth"
+        self.carrier_dir = "sslw/carriers"
+        self.device = "cuda"
+        self.num_bits = 32
+        self.data_augmentation = True
+        
+        parser = argparse.ArgumentParser()
+        
+        def aa(*args, **kwargs):
+            group.add_argument(*args, **kwargs)        
 
+        group = parser.add_argument_group('Experiments parameters')        
+        aa("--verbose", type=int, default=0)
+        
+        group = parser.add_argument_group('Marking parameters')
+        aa("--target_psnr", type=float, default=42.0, help="Target PSNR value in dB. (Default: 42 dB)")
+        aa("--target_fpr", type=float, default=1e-6, help="Target FPR of the dectector. (Default: 1e-6)")
+                
+        group = parser.add_argument_group('Optimization parameters')
+        aa("--epochs", type=int, default=100, help="Number of epochs for image optimization. (Default: 100)")
+        aa("--data_augmentation", type=str, default="all", choices=["none", "all"], help="Type of data augmentation to use at marking time. (Default: All)")
+        aa("--optimizer", type=str, default="Adam,lr=0.01", help="Optimizer to use. (Default: Adam,lr=0.01)")
+        aa("--scheduler", type=str, default=None, help="Scheduler to use. (Default: None)")
+        aa("--batch_size", type=int, default=1, help="Batch size for marking. (Default: 128)")
+        aa("--lambda_w", type=float, default=5e4, help="Weight of the watermark loss. (Default: 1.0)")
+        aa("--lambda_i", type=float, default=1.0, help="Weight of the image loss. (Default: 1.0)")
+        
+        self.params = parser.parse_args()
+        
+        if "model_path" in args: self.model_path = args["model_path"]
+        if "model_name" in args: self.model_name = args["model_name"]
+        if "normlayer_path" in args: self.model_path = args["normlayer_path"]
+        if "carrier_dir" in args: self.carrier_dir = args["carrier_dir"]
+        if "device" in args: self.device = args["device"]
+        if "wm_l" in args: self.num_bits = args["wm_l"]
+
+        if self.device != 'cpu': 
+            self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+        # Loads backbone and normalization layer
+        backbone = sslw_utils.build_backbone(path=self.model_path, name=self.model_name)
+        normlayer = sslw_utils.load_normalization_layer(path=self.normlayer_path)
+        self.model = sslw_utils.NormLayerWrapper(backbone, normlayer)
+        for p in self.model.parameters():
+            p.requires_grad = False
+        self.model.eval()
+        
+        # Load or generate carrier and angle
+        if not os.path.exists(self.carrier_dir):
+            os.makedirs(self.carrier_dir, exist_ok=True)
+        D = self.model(torch.zeros((1, 3, 224, 224)).to(self.device)).size(-1)
+        K = self.num_bits
+        carrier_path = os.path.join(self.carrier_dir,'carrier_%i_%i.pth'%(K, D))        
+        if os.path.exists(carrier_path):
+            self.carrier = torch.load(carrier_path)
+            assert D == self.carrier.shape[1]
+        else:
+            self.carrier = sslw_utils.generate_carriers(K, D, output_fpath=carrier_path)
+        self.carrier = self.carrier.to(self.device, non_blocking=True) # direction vectors of the hyperspace
+
+        if self.data_augmentation: self.data_aug = sslw_data_augmentation.All()
+        else: self.data_aug = sslw_data_augmentation.DifferentiableDataAugmentation()
+
+
+    def embed(self, tile, **args):
+        os.makedirs('sslw_tmp/sslw', exist_ok=True)
+        
+        cv2.imwrite('sslw_tmp/sslw/tmp0.png', tile)        
+        wm = args['wm']
+        
+        msgs = torch.tensor([wm])
+
+        # encoding example
+
+        dataloader = sslw_utils_img.get_dataloader('sslw_tmp', batch_size=self.params.batch_size)
+
+        pt_img = sslw_encode.watermark_multibit(dataloader, msgs, self.carrier, self.model, self.data_aug, self.params)
+        imgs_out = ToPILImage()(sslw_utils_img.unnormalize_img(pt_img[0]).squeeze(0)) 
+        imgs_out.save('tmp1.png')
+        
+        tile = cv2.imread('tmp1.png', cv2.IMREAD_COLOR)
+        
+        os.remove('sslw_tmp/sslw/tmp0.png')          
+        os.rmdir('sslw_tmp/sslw')          
+        os.rmdir('sslw_tmp')          
+        os.remove('tmp1.png')          
+
+        return tile
+
+        
+    def extract(self, tile, **args):   
+        cv2.imwrite('aux0.png', tile)
+        img = Image.open('aux0.png')
+        
+        decoded_data = sslw_decode.decode_multibit([img], self.carrier, self.model)
+        
+        os.remove('aux0.png')
+                
+        return decoded_data[0]['msg']
+
+        
 def inject_watermark(image_in, image_out='blind_watermark.jpg', wm=[True, False], use_mask=False, tile_size=None, how=None):    
     if use_mask:
         img, bbox, *_  = get_coin_image(image_in)
@@ -235,13 +397,10 @@ def inject_watermark(image_in, image_out='blind_watermark.jpg', wm=[True, False]
 
     cv2.imwrite(image_out, orig)
     
-    os.remove('tmp0.png')
-    os.remove('tmp1.png')
-
     return
 
 
-def extract_watermark(image, wm_l=2, use_mask=False, tile_size=None, how=None):
+def extract_watermark(image, wm_l=None, use_mask=False, tile_size=None, how=None):
     if use_mask:
         img, bbox, *_  = get_coin_image(image)
         img = img[bbox[2]:bbox[3], bbox[0]:bbox[1]]
@@ -311,7 +470,7 @@ attacks = [{'attack': 'none'}] \
     + [{'attack': 'sharpen', 'factor': f} for f in [1.5, 3, 7, 10]] \
     + [{'attack': 'random_noise', 'var': v} for v in [0.01, 0.02]] \
     + [{'attack': 'overlay emoji', 'emoji_size': s, 'y_pos': 0.5} for s in [0.15, 0.25, 0.35]] \
-    + [{'attack': 'overlay emoji', 'emoji_path': 'wm_logo.png', 'y_pos': y, 'x_pos': x} for y in [0.5, 0.35, 0.65] for x in [0.4, 0.6]] \
+    + [{'attack': 'overlay emoji', 'emoji_path': 'data/wm_logo.png', 'y_pos': y, 'x_pos': x} for y in [0.5, 0.35, 0.65] for x in [0.4, 0.6]] \
     + [{'attack': 'rotation', 'angle': r, 'fill': [255, 255, 255]} for r in [2, 15, 30, 45]] \
     + [{'attack': 'center crop', 'scale': s} for s in [0.25, 0.35, 0.5, 0.75, 0.8, 1.2, 1.5]] \
     + [{'attack': 'resize', 'scale': s} for s in [0.5, 0.75, 0.9, 1.1, 1.25]] \
@@ -337,8 +496,9 @@ b = 25
 # tile size
 tl = None
 
-# w_method = blind_watermarking()
-w_method = trustmark_watermarking()
+# w_method = ssl_watermarking(wm_l=l)
+# w_method = blind_watermarking(wm_l=l)
+w_method = trustmark_watermarking(wm_l=l)
 
 crop_image = True
 
@@ -387,7 +547,7 @@ for image in images:
         tw_image.save(t_image)
         im_wmrk = cv2.imread(t_image).astype(np.single)
 
-        extracted_wm = extract_watermark(t_image, wm_l=l, tile_size=tl, how=w_method)
+        extracted_wm = extract_watermark(t_image, tile_size=tl, how=w_method)
           
         if extracted_wm is None:
             extr_w = None
